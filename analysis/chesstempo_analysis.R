8aefd46123788722f829a55e366bf28ad2aec015
Isaac Verminck <isaacverm@gmail.com>
2017-01-15 20:27:11 +0100

    Answered some basic questions on chesstempo puzzles

--- analysis/chesstempo_analysis.R
+++ analysis/chesstempo_analysis.R
@@ -43,7 +43,6 @@ performance <- performance %>%
 
 performance <- performance %>% mutate(user_rating = str_extract(user_rating, "\\d{4}\\.\\d{1}"))
 
-
 ## correct data type
 
 performance <- performance %>% 
@@ -51,9 +50,38 @@ performance <- performance %>%
                           mutate_at(.cols = c("rating","user_rating"), .funs = funs(as.numeric(.))) %>%
                               mutate(problem_id = as.character(problem_id))
 
+### too hasty while solving puzzles?
 
+## distribution average solve time - own solve time
 
+# calculate difference in solve time
 
+performance <- performance %>% 
+                      mutate(diff_solve_time = solve_time - avg_time,
+                             perc_solve_time = solve_time/avg_time)
+                  
+# graph
+
+graph_diff_solve_time <- ggplot(data = performance, aes(x = diff_solve_time)) +
+                                geom_histogram()
+
+## has my hastiness changed?
+
+graph_hastiness_rating <- ggplot(data = performance, aes(x = user_rating, y = diff_solve_time)) +
+                          geom_point() +
+                          geom_smooth(method = "loess", se = FALSE)
+
+## enough time taken for difficult puzzles?
+
+# df
+
+performance <- performance %>% mutate(diff_rating = rating - user_rating)
+
+# graph
+
+graph_solve_time_difficult_puzzles <- ggplot(data = performance, aes(x = rating, y = solve_time)) +
+                                      geom_point() +
+                                      geom_smooth(method = "loess", se = FALSE)
 
 ### save
 
