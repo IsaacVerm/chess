f1ad370efdf56e75e07d1ba734c4d3cf9e7ed2ab
Isaac Verminck <isaacverm@gmail.com>
2017-01-31 20:32:46 +0100

    Rewrote cleaning chesstempo manual tactical puzzles

--- data/scripts/clean_manual_puzzles.R
+++ data/scripts/clean_manual_puzzles.R
@@ -4,8 +4,9 @@
 
 library(ggplot2)
 library(dplyr)
-library(readxl)
 library(stringr)
+library(lubridate)
+library(tidyr)
 
 ## remarks
 
@@ -17,13 +18,13 @@ rm(list=setdiff(ls(), "base_path"))
 
 # chesstempo tactical puzzles
 
-performance <- read_excel(file.path(base_path,"data","input","manual_puzzles.xlsx"))
+performance <- read.csv(file.path(base_path,"data","input","manual_puzzles.csv"), sep = ";")
 
 ### clean data
 
 ## variable names without spaces
 
-names(performance) <- c("session_id","time","problem_id","rating","type","avg_time","solve_time",
+names(performance) <- c("session_id","time","problem_id","problem_rating","type","avg_time","solve_time",
                         "time_after_first","user_rating")
 
 ## solve time information to seconds
@@ -36,16 +37,25 @@ solve_time_var <- c("avg_time","solve_time","time_after_first")
 
 performance <- performance %>% 
                   select(one_of(solve_time_var)) %>% 
-                        mutate_all(funs(2209161600 + as.numeric(.))) %>%
+                        mutate_all(funs(ms(.))) %>%
                             bind_cols(., select(performance, -one_of(solve_time_var)))
 
-## remove unnecessary rating change information
+## split rating change and user rating
 
-performance <- performance %>% mutate(user_rating = str_extract(user_rating, "\\d{4}\\.\\d{1}"))
+performance <- performance %>%
+                    separate(user_rating, c("user_rating","change_user_rating"), " ") %>%
+                          mutate(change_user_rating = str_extract(change_user_rating,
+                                                                  "(\\+|-)\\d+\\.\\d+"))
 
 ## correct data type
 
 performance <- performance %>% 
                   mutate_at(.cols = c("session_id","type"), .funs = funs(as.factor(.))) %>%
-                      mutate_at(.cols = c("rating","user_rating"), .funs = funs(as.numeric(.))) %>%
-                          mutate(problem_id = as.character(problem_id))
+                      mutate_at(.cols = c("problem_rating","user_rating","change_user_rating"),
+                                .funs = funs(as.numeric(.))) %>%
+                          mutate(problem_id = as.character(problem_id)) %>%
+                              mutate(time = dmy_hm(time))
+
+### save
+
+save(performance, file = file.path(base_path,"data","output","manual_puzzles","clean_manual_puzzles.RData"))
